c8f099f6fcfc7f9d5b21e819e355ca784f279598
João Michael - MK <88468371+Maicon-MK@users.noreply.github.com>
2023-09-28 10:56:39 -0300

    Estrutura whit para leitura de arquivos

--- estudos_em_casa/ex-casa.py
+++ estudos_em_casa/ex-casa.py
@@ -5,3 +5,9 @@ arquivo = open("nomes.txt","w" )
 arquivo.write("Michael")
 
 arquivo.close()
+
+#com whit
+with open("nomes.txt") as arquivo:
+    arquivo.write("Para escrever")
+with open("nomes.txt", 'r') as arquivo:
+    print(arquivo.read())
